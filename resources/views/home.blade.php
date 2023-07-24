ff7dd6a30b95ecbbbe680595f5e5102911744a59
Fierda Stephen <fierdastephen14@gmail.com>
2023-07-24 17:15:29 +0700

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,98 @@
 @extends('layouts.main')
 
 @section('isi')
-    <div class="container text-center">
-        <h2 class="text-3xl font-semibold">Home Page</h2>
+    <div class="text-center">
+        <h2 class="text-3xl font-semibold mb-6">Home Page</h2>
+        <div class="relative overflow-x-auto shadow-md rounded-sm border">
+            <form action="">
+                <table class="w-full text-sm text-left border">
+                    <thead class="text-xs text-gray-700 uppercase bg-red-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                No.
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Serial Number
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody id="dynamic-table">
+                        <tr>
+                            <td class="px-6 py-4">
+                                1
+                            </td>
+                            <td class="px-6 py-4">
+                                <input type="text" class="px-2.5 text-sm" name="sn" placeholder="input sn" id="input">
+                            </td>
+                            {{-- <td class="px-6 py-4">
+                                <button type="submit">+</button>
+                            </td> --}}
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="flex justify-end">
+                    <button type="submit" class="mr-6 p-2 rounded bg-green-500">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var i = 1;
+            // Listen for the 'keydown' event on input elements
+            $(document).keydown(function(event) {
+                // Check if the pressed key is Enter (key code 13)
+                if (event.keyCode === 13) {
+                    // Prevent the default form submission
+                    event.preventDefault();
+                    // Perform your desired action here
+                    // For example, you can submit a form or trigger a specific function
+                    // Replace the following line with your desired action
+                    newRow();
+                    // alert('Enter key pressed!');
+                }
+            });
+
+            function newRow() {
+                i++;
+                var newRow = `
+                <tr id="row${i}">
+                    <td class="px-6 py-4">
+                        ${i}
+                    </td>
+                    <td class="px-6 py-4">
+                        <input type="text" class="px-2.5 text-sm" name="sn${i}" placeholder="input sn">
+                    </td>
+                    <td class="px-6 py-4">
+                        <button class="btn_remove bg-red-500 px-2 rounded" id="${i}" type="button">-</button>
+                    </td>
+                </tr>
+            `;
+                $("#dynamic-table").append(newRow);
+                $("#dynamic-table tr:last-child input").focus();
+            }
+
+            $(document).on('click', '.btn_remove', function() {
+                var button_id = $(this).attr("id");
+                $('#row' + button_id + '').remove();
+                resetRowNumbers();
+            });
+
+            function resetRowNumbers() {
+            var rows = $("#dynamic-table tr");
+            for (var j = 0; j < rows.length; j++) {
+                var row = $(rows[j]);
+                row.find("td:first-child").text(j + 1);
+            }
+            i = rows.length;
+        }
+        });
+    </script>
 @endsection
---
+++ resources/views/test.blade.php
@@ -0,0 +1,101 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32">
+    <title>Dasarata Login</title>
+    <style>
+        @media (max-width: 500px) {
+            .flex {
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: screen;
+            }
+
+            .w-96 {
+                width: 80%;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex h-screen w-screen justify-center items-center bg-no-repeat bg-center bg-cover"
+        style="background-image: url('/img/bg5.png');">
+        <div
+            class="flex columns-1 w-96 h-auto bg-black border border-indigo-500 bg-opacity-10 rounded-3xl backdrop-blur-xl items-center">
+            <div class="w-96 mx-12 my-8">
+                <div class="flex justify-center">
+                    <img src="/img/logo-white.png" alt="Logo" class="w-48">
+                </div>
+                <h2 class="block text-white font-sans font-semibold text-2xl mt-4">Login</h2>
+                <form action="#">
+                    <div class="mt-6">
+                        <div class="mb-6">
+                            <label for="email"
+                                class="block text-sm mb-1 font-light font-sans leading-none text-white dark:text-white">User</label>
+                            <input type="email" name="email" id="email" value="{{ old('email') }}"
+                                class="bg-slate-50 border border-gray-300 text-grey-900 text-sm rounded-lg focus:ring-darkBlue focus:border-darkBlue block w-full px-4 py-2 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                                placeholder="username@gmail.com" required autocomplete="off">
+                        </div>
+                        <div class="mb-6">
+                            <label for="password"
+                                class="block text-sm mb-1 font-light font-sans leading-none text-white dark:text-white">Password</label>
+                            <input type="password" name="password" id="password"
+                                class="bg-slate-50 border border-gray-300 text-grey-900 text-sm rounded-lg focus:ring-darkBlue focus:border-darkBlue block w-full px-4 py-2 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                                placeholder="Password" required>
+                        </div>
+                        <button type="submit"
+                            class="text-white font-bold bg-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none rounded-lg text-sm w-full px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Sign
+                            in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="flex h-screen w-screen justify-center px-16 py-8 bg-cover bg-center bg-no-repeat" style="background-image: url('/img/bg5.png');">
+        <div class="columns-1 h-full flex justify-center border border-indigo-500 bg-indigo-500 bg-opacity-10 rounded-3xl backdrop-blur-xl">
+            <div class="grid grid-cols-2 gap-2">
+                <div class="justify-center items-center inline-flex rounded-3xl">
+                    <img src="https://source.unsplash.com/400x300/?networking" alt="Network"
+                        class="w-full h-full rounded-r-3xl">
+                </div>
+                <div class="flex justify-center items-center py-8">
+                    <div class="w-96 h-96 p-6">
+                        <img src="/img/logo-white.png" alt="Logo" class="w-48">
+                        <form action="#" class="mt-6">
+                            <div class="my-3">
+                                <label for="email"
+                                    class="block text-sm mb-1 font-normal font-sans leading-none text-white dark:text-white">User</label>
+                                <input type="email" name="email" id="email" value="{{ old('email') }}"
+                                    class="bg-slate-50 border border-gray-300 text-grey-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 @error('email') is-invalid @enderror"
+                                    placeholder="joko@gmail.com" required>
+                            </div>
+                            <div class="my-3">
+                                <label for="password"
+                                    class="block text-sm mb-1 font-normal text-white dark:text-white">Password</label>
+                                <input type="password" name="password" id="password"
+                                    class="bg-slate-50 border border-gray-300 text-grey-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                                    required>
+                            </div>
+                            <div class="text-end text-sm my-3">
+                                <a href="#" class="text-white">Forgot Password?</a>
+                            </div>
+                            <button>
+                                <button type="submit"
+                                    class="text-white bg-DarkBlue hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full px-4 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Login</button>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+</body>
+
+</html>
